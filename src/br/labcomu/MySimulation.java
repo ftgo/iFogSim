bebadd6ac52d668dc9bbc7fe7f50f426b5584890
Felipe Toledo G Oliveira <felipetgoliveira@gmail.com>
2021-05-28 11:11:50 -0300

    improved properties

--- src/br/labcomu/MySimulation.java
+++ src/br/labcomu/MySimulation.java
@@ -24,22 +24,39 @@ import static org.fog.entities.Tuple.DOWN;
 import static org.fog.entities.Tuple.UP;
 
 public class MySimulation extends Simulation {
-	
-	private int latency_endDevicetoSwitch;
-	private int latency_switchtoSwitch;
-	private int latency_switchtoFogDevice;
-	private int latency_switchtoCloudDevice;
-	private int bandwith;
-	
-	public MySimulation(int latency_endDevicetoSwitch, int latency_switchtoSwitch, int latency_switchtoFogDevice,
-			int latency_switchtoCloudDevice, int bandwith) {
-		super();
-		this.latency_endDevicetoSwitch = latency_endDevicetoSwitch;
-		this.latency_switchtoSwitch = latency_switchtoSwitch;
-		this.latency_switchtoFogDevice = latency_switchtoFogDevice;
-		this.latency_switchtoCloudDevice = latency_switchtoCloudDevice;
-		this.bandwith = bandwith;
-	}
+    private long mips_device;
+    private int ram_device;
+    private double tupleCpuLength_sensor_device;
+    private double tupleCpuLength_device_actuator;
+    private double tupleNwLength_sensor_device;
+    private double tupleNwLength_device_actuator;
+    private double selectivity_device_sensor_actuator;
+    private double latency_iot_switch0;
+    private double latency_switch0_switch1;
+    private double latency_switch0_fog;
+    private double latency_switch1_switch2;
+    private double latency_switch2_cloud;
+    private double bandwidth;
+
+    public MySimulation() {
+        this(1000, 100, 30000.0, 10 * 1024, 1000.0, 1 * 1024, 1.0, 10.0, 2.0, 15.0, 20.0, 2.0, 1000.0);
+    }
+
+    public MySimulation(long mips_device, int ram_device, double tupleCpuLength_sensor_device, double tupleNwLength_sensor_device, double tupleCpuLength_device_actuator, double tupleNwLength_device_actuator, double selectivity_device_sensor_actuator, double latency_iot_switch0, double latency_switch0_fog, double latency_switch0_switch1, double latency_switch1_switch2, double latency_switch2_cloud, double bandwidth) {
+        this.mips_device = mips_device;
+        this.ram_device = ram_device;
+        this.tupleCpuLength_sensor_device = tupleCpuLength_sensor_device;
+        this.tupleCpuLength_device_actuator = tupleCpuLength_device_actuator;
+        this.tupleNwLength_sensor_device = tupleNwLength_sensor_device;
+        this.tupleNwLength_device_actuator = tupleNwLength_device_actuator;
+        this.selectivity_device_sensor_actuator = selectivity_device_sensor_actuator;
+        this.latency_iot_switch0 = latency_iot_switch0;
+        this.latency_switch0_fog = latency_switch0_fog;
+        this.latency_switch0_switch1 = latency_switch0_switch1;
+        this.latency_switch1_switch2 = latency_switch1_switch2;
+        this.latency_switch2_cloud = latency_switch2_cloud;
+        this.bandwidth = bandwidth;
+    }
 
     /*
      * |    MODULE
@@ -54,18 +71,19 @@ public class MySimulation extends Simulation {
         /*
          * Adding modules (vertices) to the application infra (directed graph)
          */
-        application.addAppModule(FOG_DEVICE_TYPE, 1000, 100);
+        application.addAppModule(FOG_DEVICE_TYPE, this.mips_device, this.ram_device);
+
 
         /*
          * Connecting the application modules (vertices) in the application infra (directed graph) with edges
          */
-        application.addAppEdge(SENSOR_TYPE, FOG_DEVICE_TYPE, 30000, 10 * 1024, SENSOR_TYPE, UP, SENSOR);
-        application.addAppEdge(FOG_DEVICE_TYPE, ACTUATOR_TYPE, 1000, 1 * 1024, ACTUATOR_TYPE, DOWN, ACTUATOR);  // adding edge from Client module to Display (actuator) carrying tuples of type SELF_STATE_UPDATE
+        application.addAppEdge(SENSOR_TYPE, FOG_DEVICE_TYPE, this.tupleCpuLength_sensor_device, this.tupleNwLength_sensor_device, SENSOR_TYPE, UP, SENSOR);
+        application.addAppEdge(FOG_DEVICE_TYPE, ACTUATOR_TYPE, this.tupleCpuLength_device_actuator, this.tupleNwLength_device_actuator, ACTUATOR_TYPE, DOWN, ACTUATOR);  // adding edge from Client module to Display (actuator) carrying tuples of type SELF_STATE_UPDATE
 
         /*
          * Defining the input-output relationships (represented by selectivity) of the application modules.
          */
-        application.addTupleMapping(FOG_DEVICE_TYPE, SENSOR_TYPE, ACTUATOR_TYPE, new FractionalSelectivity(1.0)); // TODO FractionalSelectivity?
+        application.addTupleMapping(FOG_DEVICE_TYPE, SENSOR_TYPE, ACTUATOR_TYPE, new FractionalSelectivity(this.selectivity_device_sensor_actuator)); // TODO FractionalSelectivity?
 
 
         ArrayList<String> modules = new ArrayList<String>();
@@ -113,74 +131,53 @@ public class MySimulation extends Simulation {
         Switch switch0 = new EdgeSwitch("SWITCH0");
         topology.addSwitch(switch0);
 
-        topology.addLink(iot.getId(), switch0.getId(), getLatency_endDevicetoSwitch(), getBandwith()); // [2]
+        topology.addLink(iot.getId(), switch0.getId(), (int) this.latency_iot_switch0, (int) this.bandwidth); // [2]
 
         FogDevice fog = fogDeviceBuilder.setCloud(false).build("FOG");
         topology.addFogDevice(fog);
 
-        topology.addLink(switch0.getId(), fog.getId(), getLatency_switchtoFogDevice(), getBandwith()); // [3]
+        topology.addLink(switch0.getId(), fog.getId(), (int) this.latency_switch0_fog, (int) this.bandwidth); // [3]
 
         Switch switch1 = new Switch("SWITCH1");
         topology.addSwitch(switch1);
 
-        topology.addLink(switch0.getId(), switch1.getId(), getLatency_switchtoSwitch(), getBandwith()); // [4]
+        topology.addLink(switch0.getId(), switch1.getId(), (int) this.latency_switch0_switch1, (int) this.bandwidth); // [4]
 
         Switch switch2 = new Switch("SWITCH2");
         topology.addSwitch(switch2);
 
-        topology.addLink(switch1.getId(), switch2.getId(), getLatency_switchtoSwitch(), getBandwith()); // [5]
+        topology.addLink(switch1.getId(), switch2.getId(), (int) this.latency_switch1_switch2, (int) this.bandwidth); // [5]
 
         FogDevice cloud = fogDeviceBuilder.setCloud(true).build("CLOUD");
         topology.addFogDevice(cloud);
 
-        topology.addLink(switch2.getId(), cloud.getId(), getLatency_switchtoCloudDevice(), getBandwith()); // [6]
+        topology.addLink(switch2.getId(), cloud.getId(), (int) this.latency_switch2_cloud, (int) this.bandwidth); // [6]
     }
 
-    public int getLatency_endDevicetoSwitch() {
-		return latency_endDevicetoSwitch;
-	}
+    public static void main(String[] args) throws Exception {
+        double latency_switch0_switch1 = 15.0;
+        double latency_switch1_switch2 = 20.0;
 
-	public void setLatency_endDevicetoSwitch(int latency_endDevicetoSwitch) {
-		this.latency_endDevicetoSwitch = latency_endDevicetoSwitch;
-	}
+        Simulation simulation = new MySimulation(
+                1000,
+                100,
+                30000.0,
+                10 * 1024,
+                1000.0,
+                1 * 1024,
+                1.0,
+                10.0,
+                2.0,
+                latency_switch0_switch1, // 15.0
+                latency_switch1_switch2, // 20.0
+                2.0,
+                1000.0
+        );
 
-	public int getLatency_switchtoSwitch() {
-		return latency_switchtoSwitch;
-	}
+        simulation.switchLog(false);
 
-	public void setLatency_switchtoSwitch(int latency_switchtoSwitch) {
-		this.latency_switchtoSwitch = latency_switchtoSwitch;
-	}
+        simulation.initialize();
 
-	public int getLatency_switchtoFogDevice() {
-		return latency_switchtoFogDevice;
-	}
-
-	public void setLatency_switchtoFogDevice(int latency_switchtoFogDevice) {
-		this.latency_switchtoFogDevice = latency_switchtoFogDevice;
-	}
-
-	public int getLatency_switchtoCloudDevice() {
-		return latency_switchtoCloudDevice;
-	}
-
-	public void setLatency_switchtoCloudDevice(int latency_switchtoCloudDevice) {
-		this.latency_switchtoCloudDevice = latency_switchtoCloudDevice;
-	}
-
-	public int getBandwith() {
-		return bandwith;
-	}
-
-	public void setBandwith(int bandwith) {
-		this.bandwith = bandwith;
-	}
-
-	public static void main(String[] args) throws Exception {
-		Simulation simulation = new MySimulation(10, 50, 20, 500, 100);
-		simulation.switchLog(false);
-		simulation.initialize();
-		
-		simulation.run();
+        simulation.run();
     }
 }
